2ee684777fdcb108bf37b2cb5df5090b905c9a79
Mute <j0803@naver.com>
2020-03-01 19:00:17 +0900

    원형탄 타입1, 2 템플릿

---
+++ BulletAlgorithm/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 BulletPos;
+    private Vector2 SpeedVector;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(SpeedVector * Time.deltaTime);
+    }
+
+    public void InitBullet(Vector2 Pos, Vector2 Speed)
+    {
+        BulletPos = Pos;
+        SpeedVector = Speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Overlap")  // if you using Object pooling, Just Refund it.
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BulletAlgorithm/Assets/Scripts/CircleBullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBullet : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+
+    private float theta = 0;
+
+    private Vector2 ActorPos;
+    private Vector2 BulletPos;
+
+    public float thetaStep; // 공차 각도
+    public float bulletSpeed;
+
+    void Start()
+    {
+        StartCoroutine(BulletGenerator());
+    }
+
+    IEnumerator BulletGenerator()
+    {
+        while(true)
+        {
+           MakeBullet();
+           yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+
+    private void MakeBullet()
+    {
+        ActorPos = gameObject.transform.position;
+        BulletPos = ActorPos;
+
+        if(theta + thetaStep >= 360)
+            theta = 0;
+        else
+            theta += thetaStep;        
+
+        GameObject newBullet = Instantiate(BulletPrefab,
+        new Vector3(0,0,0), new Quaternion(0,0,0,0)) as GameObject;
+        newBullet.GetComponent<Bullet>().InitBullet(BulletPos, InitDirectBullet.GetDirectVector(theta) * bulletSpeed);    
+    }
+}
