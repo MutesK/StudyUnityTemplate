59897a331eba083d234092923df1e3f56a778541
Mute <j0803@naver.com>
2020-03-04 14:48:23 +0900

    PlayerController 조작 처리 및 충돌 감지후 깜빡힘 효과 추가

--- BulletAlgorithm/Assets/Scripts/PlayerController.cs
+++ BulletAlgorithm/Assets/Scripts/PlayerController.cs
@@ -3,26 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * Player 움직임 정
+ *  Project Setting에 있는 Input Manager에 키 값을 받아 움직이게 합니다.
+ *  모든 플레이어 관련 로직처리는 이곳에 있는게 좋습니다.
  */
 public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed;
 
+    public HitEffect Effect;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Effect.InitEffect(GetComponent<SpriteRenderer>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
 
-    void FixedUpdate()
+    void Movement()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(horizontal, vertical, 0) * MoveSpeed
+            * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("OnHit");
+            Effect.StartEffect();
+        }
     }
 }
