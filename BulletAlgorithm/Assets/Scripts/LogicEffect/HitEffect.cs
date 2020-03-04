8de0d9412df1f8e897e56c331a48321e66bdb7f5
Mute <j0803@naver.com>
2020-03-04 14:49:32 +0900

    미업로드 파일 업로드, 차후 gitignore 추가하여 필요없는 파일 제거예정.

---
+++ BulletAlgorithm/Assets/Scripts/LogicEffect/HitEffect.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour
+{
+    SpriteRenderer Sprite;
+    float EffectTime = .0f;
+    public float FlickTime;
+    public float DurationTime;
+    bool isStop = true;
+
+    public void InitEffect(SpriteRenderer RenderSprite)
+    {
+        Sprite = RenderSprite;
+    }
+
+    public void StartEffect()
+    {
+        if (isStop)
+        {
+            isStop = false;
+
+            Invoke("OnFlickSprite", 0f);
+        }
+    }
+
+    public void ForceStopEffect()
+    {
+        isStop = true;
+    }
+
+    public bool isEffecting()
+    {
+        return isStop;
+    }
+
+    private void Update()
+    {
+        if(!isStop)
+        {
+            EffectTime = Time.deltaTime;
+        }
+    }
+
+    void OnFlickSprite()
+    { 
+        if(isStop || DurationTime <= EffectTime)
+        {
+            Sprite.color = new Color(1, 1, 1, 1);
+            isStop = true;
+            return;
+        }
+
+        Sprite.color = new Color(1, 1, 1, 0.4f);
+        Invoke("OffFlickSprite", 0.5f);
+    }
+
+    void OffFlickSprite()
+    {
+        Sprite.color = new Color(1,1,1,1);
+        Invoke("OnFlickSprite", 0.5f);
+    }
+}
